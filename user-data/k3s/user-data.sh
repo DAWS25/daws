dd15ded3e4e00da30b272b666cfe36909ae50428
Julio Faerman <356476+faermanj@users.noreply.github.com>
2026-03-27 06:57:03 +0100

    fix: resolve merge conflict in user-data/k3s/user-data.sh

--- user-data/k3s/user-data.sh
+++ user-data/k3s/user-data.sh
@@ -19,9 +19,6 @@ retry() {
 echo "[$(date -Iseconds)] Setting up k3s on Amazon Linux 2023 [1357]"
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-<<<<<<< HEAD
-ARTIFACTS_BUCKET=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$(ec2-metadata --instance-id | cut -d ' ' -f 2)" "Name=key,Values=artifacts-bucket" --query 'Tags[0].Value' --output text)
-=======
 DEFAULT_ARTIFACTS_BUCKET="gitops-artifacts-${ACCOUNT_ID}"
 
 IMDS_TOKEN=$(curl -sS -X PUT "http://169.254.169.254/latest/api/token" \
@@ -47,7 +44,7 @@ else
   ARTIFACTS_BUCKET="${DEFAULT_ARTIFACTS_BUCKET}"
   echo "Using default ARTIFACTS_BUCKET: ${ARTIFACTS_BUCKET}"
 fi
->>>>>>> 752c53a (chore: workspace sync 2026-03-27)
+
 LOG_FILE="/var/log/cloud-init-output.log"
 
 # echo "Installing base dependencies"
